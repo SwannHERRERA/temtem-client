7592fdbdd8bf39a6e6c339549cc1066b40698680
HERRERA Swann <swann@devloup.Dev>
2020-09-24 23:16:33 +0200

    First Commit

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'temtem-client',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Rancho&display=swap',
+      },
+    ],
+    script: [
+      {
+        src:
+          'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',
+        integrity:
+          'sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo',
+        crossorigin: 'anonymous',
+      },
+      {
+        src: '~/assets/js/bootstrap.bundle.min.js',
+      },
+    ],
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: ['@assets/css/bootstrap.min.css'],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [{ src: '~/plugins/Vuelidate' }],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/stylelint
+    '@nuxtjs/stylelint-module',
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {},
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {},
+}
---
+++ pages/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container">
+    <div class="card mt-4">
+      <div class="card-header bg-light">
+        <h1 class="title">Login</h1>
+      </div>
+      <div class="card-body">
+        <form>
+          <div class="mb-3">
+            <label for="email">email</label>
+            <input
+              id="email"
+              v-model.trim="$v.form.email.$model"
+              type="text"
+              class="form-control"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="password">password</label>
+            <input
+              id="password"
+              v-model="$v.form.password.$model"
+              type="text"
+              :class="[
+                { 'is-invalid': $v.form.password.$error },
+                'form-control',
+              ]"
+            />
+            <div class="invalid-feedback">This field is required</div>
+          </div>
+          <p class="text-center">
+            <button class="btn btn-primary">Se connecter</button>
+          </p>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { required, email } from 'vuelidate/lib/validators'
+
+export default {
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+      },
+    }
+  },
+  validations: {
+    form: {
+      email: {
+        required,
+        email,
+      },
+      password: {
+        required,
+      },
+    },
+  },
+}
+</script>
+<style>
+.title {
+  font-family: 'Rancho', cursive;
+  font-size: 4rem;
+  text-align: center;
+}
+</style>
